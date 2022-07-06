47063d6ffdbc2e36ae1f23d9d066e0f0cdafcd53
DraZe <pierrealiasdraze@gmail.com>
2022-07-06 23:38:47 +0200

    Verif extension + opti client

--- client/src/views/ImageUploader.vue
+++ client/src/views/ImageUploader.vue
@@ -4,14 +4,14 @@
           <h1 class="font-bold text-2xl my-6">Upload your image</h1>
           <h3 class="text-md my-6">File should be Jpeg, Png, ...</h3>
       </div>
-      <div id="upload" @click="openFileDialog" @drop.prevent="addDragNDropFile" class="hover:cursor-pointer bg-gray-100 border-2 border-dashed border-blue-300 mx-6 rounded-lg">
+      <div id="upload" @click="openFileDialog" @drop.prevent="addFile" class="hover:cursor-pointer bg-gray-100 border-2 border-dashed border-blue-300 mx-6 rounded-lg">
             <img class="m-auto my-6" width="200" src="@/assets/image.svg" alt="upload-img">
             <p class="text-gray-400 my-12">Drag & Drop your image here</p>
       </div>
       <div id="footer">
           <p class="text-gray-400 mt-6">Or</p>
           <input @click="openFileDialog" type="button" value="Choose a file" class="hover:cursor-pointer bg-blue-600 text-white my-6 rounded-lg py-2 px-4">
-          <input @change="newFile" type="file" class="hidden" id="hiddenFileButton">
+          <input @change="addFile" type="file" class="hidden" id="hiddenFileButton">
       </div>
   </div>
     <UploadingPage v-if="isUploading" :progress="uploadProgess" />
@@ -39,56 +39,41 @@ export default {
     },
     mounted() {
         const events = ['dragenter', 'dragover', 'dragleave', 'drop'];
-        events.forEach((event) => {
+        events.forEach((event) => { // Retirer le comportement par défaut qui fait qu'on copie l'image sur le navigateur.
             document.addEventListener(event, (e) => {
                 e.preventDefault();
             })
         })
     },
     methods: {
-        openFileDialog() {
+        openFileDialog() { // Ouvrir le gestionnaire de fichier pour séléctionner une image
             const fileButton = document.getElementById("hiddenFileButton");
             fileButton.click();
         },
-        newFile() {
-            this.isUploading = true;
-            const fileButton = document.getElementById("hiddenFileButton");
-            const formData = new FormData();
-            formData.append("file", fileButton.files[0]);
-            this.file = fileButton.files[0];
-            console.log(this.file);
-            axios.post("http://localhost:4000/upload", formData, {
-                onUploadProgress: Progess => {
-                    let progess = Math.round((Progess.loaded / Progess.total) * 100);
-                    this.uploadProgess = progess;
-                }
-            }).then( res => {
-                console.log(res);
-                this.isUploading = false;
-                this.uploadEnded = true;
-            }).catch(err => {
-                console.log(err);
-            })
-        },
-        addDragNDropFile(e) {
-            const file = e.dataTransfer.files[0];
+        addFile(event) {
+            let file;
+            if (event.dataTransfer) { // Si c'est par drag'n drop
+                file = event.dataTransfer.files[0];
+            } else { // Si c'est par le biais des boutons
+                const fileButton = document.getElementById("hiddenFileButton");
+                file = fileButton.files[0];
+            }
             this.isUploading = true;
             const formData = new FormData();
             formData.append("file", file);
             this.file = file;
             axios.post("http://localhost:4000/upload", formData, {
                 onUploadProgress: Progess => {
-                    let progess = Math.round((Progess.loaded / Progess.total) * 100);
+                    let progess = Math.round((Progess.loaded / Progess.total) * 100); // Etat de l'upload, ici quasi instantané car en local
                     this.uploadProgess = progess;
                 }
-            }).then( res => {
-                console.log(res);
+            }).then(() => { // réussite
                 this.isUploading = false;
                 this.uploadEnded = true;
-            }).catch(err => {
+            }).catch(err => { // Erreur
                 console.log(err);
             })
-        }
+        },
     }
 }
 </script>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const PORT = 4000
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
+const extensions = /(\.jpg|\.jpeg|\.png|\.gif|\.svg)$/i
 
 const app = express();
 
@@ -15,14 +16,18 @@ app.post('/upload', (req, res) => {
     if (!req.files) return res.status(500).send({ msg: "file is not found" })
 
     const file = req.files.file
-    file.mv(`${__dirname}/public/${file.name}`, function (err) {
-        if (err) {
-            console.log(err)
-            return res.status(500).send({ msg: "Error occured" });
-        }
-        
-        return res.send({name: file.name, path: `/${file.name}`});
-    });
+    if (extensions.exec(file.name)) { // vérifie l'extension du fichier pour vérifier si c'est une image
+        file.mv(`${__dirname}/public/${file.name}`, function (err) {
+            if (err) {
+                console.log(err)
+                return res.status(500).send({ msg: "Error occured" });
+            }
+            
+            return res.send({name: file.name, path: `/${file.name}`});
+        });
+    } else { // Pas une image ou alors pas le bon type
+        res.status(500).send({ msg: "Wrong type" })
+    }
 })
 
 app.listen(PORT, () => {
